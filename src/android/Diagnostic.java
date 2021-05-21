fe6781bdad0bf03ae95ee22b63533d33e06df5ea
Ram Mehta <ram.mehta@gmail.com>
2021-05-21 01:25:19 -0400

    Simplify restart logic.

--- src/android/Diagnostic.java
+++ src/android/Diagnostic.java
@@ -696,17 +696,9 @@ public class Diagnostic extends CordovaPlugin{
                             c.getPackageName()
                     );
                     if (mStartActivity != null) {
-                        //mStartActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        //create a pending intent so the application is restarted after System.exit(0) was called.
-                        // We use an AlarmManager to call this intent in 100ms
-                        int mPendingIntentId = 223344;
-                        PendingIntent mPendingIntent = PendingIntent
-                                .getActivity(c, mPendingIntentId, mStartActivity,
-                                        PendingIntent.FLAG_CANCEL_CURRENT);
-                        AlarmManager mgr = (AlarmManager) c.getSystemService(Context.ALARM_SERVICE);
-                        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
+                        mStartActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        c.getApplicationContext().startActivity(mStartActivity);
                         Log.i(TAG,"Killing application for cold restart");
-                        //kill the application
                         System.exit(0);
                     } else {
                         handleError(baseError+"StartActivity is null");
